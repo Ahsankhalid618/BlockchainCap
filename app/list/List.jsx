8e71d086dd0718ad5c03d4069df99934cdb5798a
Ahsan khalid <ahsankhalid816@gmail.com>
2023-09-04 03:48:48 +0500

    New table created

--- app/list/List.jsx
+++ app/list/List.jsx
@@ -1,12 +1,61 @@
-import React from 'react'
-import styles from "./list.module.css"
+import React from "react";
+import styles from "./list.module.css";
 
 function List() {
   return (
-    <>
-    List display
-    </>
-  )
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <table className={styles.table}>
+          <thead className={styles.thead}>
+            <tr className={styles.trow}>
+              <td className={styles.tdata}>#</td>
+              <td className={styles.tdata}>Name</td>
+              <td className={styles.tdata}>Symbol</td>
+              <td className={styles.tdata}>Price</td>
+            </tr>
+          </thead>
+          <tbody className={styles.tbody}>
+            <tr className={styles.trow}>
+              <td className={styles.tdata}>01</td>
+              <td className={styles.tdata}>Bitcoin</td>
+              <td className={styles.tdata}>BTC</td>
+              <td className={styles.tdata}>26540</td>
+            </tr>
+            <tr className={styles.trow}>
+              <td className={styles.tdata}>01</td>
+              <td className={styles.tdata}>Bitcoin</td>
+              <td className={styles.tdata}>BTC</td>
+              <td className={styles.tdata}>26540</td>
+            </tr>
+            <tr className={styles.trow}>
+              <td className={styles.tdata}>01</td>
+              <td className={styles.tdata}>Bitcoin</td>
+              <td className={styles.tdata}>BTC</td>
+              <td className={styles.tdata}>26540</td>
+            </tr>
+            <tr className={styles.trow}>
+              <td className={styles.tdata}>01</td>
+              <td className={styles.tdata}>Bitcoin</td>
+              <td className={styles.tdata}>BTC</td>
+              <td className={styles.tdata}>26540</td>
+            </tr>
+            <tr className={styles.trow}>
+              <td className={styles.tdata}>01</td>
+              <td className={styles.tdata}>Bitcoin</td>
+              <td className={styles.tdata}>BTC</td>
+              <td className={styles.tdata}>26540</td>
+            </tr>
+            <tr className={styles.trow}>
+              <td className={styles.tdata}>01</td>
+              <td className={styles.tdata}>Bitcoin</td>
+              <td className={styles.tdata}>BTC</td>
+              <td className={styles.tdata}>26540</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default List
+export default List;
